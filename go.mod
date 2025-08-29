94249a445649328216426a172cf4dbfcc7bf8263
Shrv Bldna <151522619+ashrv12@users.noreply.github.com>
2025-08-29 22:36:50 +0800

    Setup HTTP and CORS rules

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/ashrv12/p04-goscraper
 
 go 1.25.0
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/go-chi/cors v1.2.2
--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("Port is not bound in the env.")
+	}
+
+	// setup a new gochi router
+	router := chi.NewRouter()
+
+	// setup permissive cors rules
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server is running on localhost:%v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
